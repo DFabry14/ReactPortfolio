ed44717cb8795637ccb3c81a45ec9986a9ce91e2
Dan Fabry <dfabry14@gmail.com>
2019-02-11 16:59:25 -0600

    done for today

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Parallax } from "react-materialize"
+import { Card, CardTitle, Col } from 'react-materialize';
 import "./Main.css"
 
 const MainPage = () => (
     <div>
-        <div className="section white">
-            <div className="row container">
-                <h2 className="header">Chicago-based Full-Stack Web Developer</h2>
-                <p className="grey-text text-darken-3 lighten-3">Dan is a Chicago-Based web developer and recent graduate of the Northwestern University School of Professional Studies Full-Stack Web Development Course.</p>
-            </div>
-        </div>
-        <Parallax imageSrc="https://github.com/DFabry14/ReactPortfolio/blob/master/src/images/cityview.jpg?raw=true" />
-        <div className="section white">
-            <div className="row container">
-                <h2 className="header">Skills</h2>
-                <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
-            </div>
-        </div>
-        <Parallax imageSrc="https://github.com/DFabry14/ReactPortfolio/blob/master/src/images/playpen.jpg?raw=true" />
+        <h4>Dan is a Chicago-based web developer and recent graduate of the Northwestern University School of Professional Studies Full-Stack Web Development Course.</h4>
+        <h4>Skills</h4>
+        <ul>HTML</ul>
+        <ul>JavaScript</ul>
+        <ul>CSS</ul>
+        <ul>React.js</ul>
+        <ul>Node.js</ul>
+        {/* <Col s={3} m={5}>
+            <Card header={<CardTitle reveal image={"https://github.com/DFabry14/ReactPortfolio/blob/master/src/images/ACC.png?raw=true"} waves='light' />}
+                title="Aaron's Coffee Corner"
+                reveal={<p>Materialize-based website for the Aaron's Coffee Corner charity. Uses Google Analytics.</p>}>
+                <p><a href="http://www.aaronscoffeecorner.org/" target="_blank">Check it out!</a></p>
+            </Card>
+        </Col> */}
     </div>
 )
 
