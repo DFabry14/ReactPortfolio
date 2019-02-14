8d9e76e01422ab563636bf4003c2866da8099e81
Dan Fabry <dfabry14@gmail.com>
2019-02-13 18:53:21 -0600

    deleted CNAME

---
+++ build/precache-manifest.3237b299fa93530b47061d8e7d65e5db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0d3bce3cdb73fe5224a",
+    "url": "/static/js/main.d0d3bce3.chunk.js"
+  },
+  {
+    "revision": "0550eb35351346be9bad",
+    "url": "/static/js/1.0550eb35.chunk.js"
+  },
+  {
+    "revision": "d0d3bce3cdb73fe5224a",
+    "url": "/static/css/main.248ce096.chunk.css"
+  },
+  {
+    "revision": "42113311949b8fb5c52c431f56067e06",
+    "url": "/index.html"
+  }
+];
